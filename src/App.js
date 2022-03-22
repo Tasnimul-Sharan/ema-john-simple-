bfcb29b85f412cf0386e21aea692469c69ce0a28
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-03-22 10:49:45 +0600

    started e-commerce website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import logo from './images/Logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header></Header>
+     <Shop></Shop>
     </div>
   );
+}
+
+const Header = () => {
+return(
+  <nav className='header'>
+    <img src={logo} alt="" />
+    <div>
+    <a href="/Home">Home</a>
+    <a href="/Orders">Orders</a>
+    <a href="/Inventory">Inventory</a>
+    <a href="/About">About</a>
+    </div>
+  </nav>
+)
+}
+
+const Shop = () => {
+  return (
+    <div className='shop-container'>
+       <div className='products-container'>
+         <h3>this is product </h3>
+        </div>
+        <div className='cart-container'>
+      <h3>this is cart</h3>
+        </div>
+    </div>
+  )
 }
 
 export default App;
