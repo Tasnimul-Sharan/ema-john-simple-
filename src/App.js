b071e5f8c3297d24cc7d4448fa7819da33afd3bd
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-03-22 12:25:26 +0600

    product picture set

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './images/Logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -25,14 +26,32 @@ return(
 }
 
 const Shop = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch('products.json')
+    .then(res => res.json())
+    .then(data => setProducts(data))
+  }, [])
   return (
     <div className='shop-container'>
        <div className='products-container'>
-         <h3>this is product </h3>
+         {
+           products.map(product => <Product key={product.id} product={product}></Product>)
+         }
         </div>
         <div className='cart-container'>
       <h3>this is cart</h3>
         </div>
+    </div>
+  )
+}
+
+const Product = (props) => {
+  const {name, img, seller, price, ratings} = props.product ;
+  return(
+    <div className='product'>
+      <img src={img} alt="" />
     </div>
   )
 }
