387dce1fad06dfc3ca833181e8feb34b86148dc2
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-03-24 02:11:23 +0600

    facing some problem

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import { useEffect, useState } from 'react';
+import {addToDb} from './utilities/fakedb';
 
 function App() {
   return (
@@ -37,11 +38,26 @@ const Shop = () => {
     .then(data => setProducts(data))
   }, [])
 
+  useEffect(() => {
+    const storedCart =addToDb();
+    const saveCart = [] ;
+    for(const id in storedCart){
+      const addProduct = products.find(product => product.id === id )
+      console.log(addProduct)
+      if(addProduct){
+        const quantity = storedCart[id];
+        addProduct.quantity = quantity;
+        saveCart.push(addProduct)
+      }
+    }
+
+  }, [products])
+
   const handleAddToCart = (product) => {
     console.log(product)
     // const newCart = [...cart, product]
     setCart([...cart, product])
-
+    addToDb(product.id)
   }
 
   return (
@@ -53,14 +69,6 @@ const Shop = () => {
         </div>
         <div className='cart-container'>
           <Cart cart={cart}></Cart>
-          {/* <h2>Order Summary</h2>
-          <p>Selected Items : {cart.length}</p> */}
-          {/* <p>Total Price:</p>
-          <p>Total Shipping Charge:</p>
-          <p>Tax:</p>
-          <h2>Grand Total:</h2>
-          <button>Clear Cart</button> <br />
-          <button>Review Order</button> */}
         </div>
     </div>
   )
@@ -87,10 +95,26 @@ const Product = (props) => {
 }
 
 const Cart = ({cart}) => {
+
+  let total = 0 ;
+  let shipping = 0
+  for(const product of cart){
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+  const tax = parseFloat((total * 0.1).toFixed(2))
+  const grandTotal = total + shipping + tax ;
   return(
-    <div>
+    <div className='cart'>
       <h2>Order Summary</h2>
       <p>Selected Items : {cart.length}</p>
+      <p>Total Price : $ {total}</p>
+      <p>Total Shipping :$ {shipping}</p>
+      <p>Tax : $ {tax}</p>
+      <h5>Grand Total: $ {grandTotal}</h5>
+      <button>Clear Cart</button> <br />
+      <button>Review Order</button>
+      <p></p>
     </div>
   )
 }
--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -7,18 +7,29 @@ const addToDb = id =>{
     if(storedCart){
         shoppingCart = JSON.parse(storedCart);
     }
-
     // add quantity
     const quantity = shoppingCart[id];
     if(quantity){
-        const newQuantity = quantity + 1;
-        shoppingCart[id] = newQuantity;
+        // const newQuantity = quantity + 1;
+        shoppingCart[id] = quantity + 1;
     }
     else{
         shoppingCart[id] = 1;
     }
     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
 }
+
+// const getStoredCart = () => {
+//     let shoppingCart = {};
+
+//     //get the shopping cart from local storage
+//     const storedCart = localStorage.getItem('shopping-cart');
+//     if(storedCart){
+//         shoppingCart = JSON.parse(storedCart);
+//     }
+//    return shoppingCart;
+
+// }
 
 const removeFromDb = id =>{
     const storedCart = localStorage.getItem('shopping-cart');
